8b3a5cc432bcd295c4f8fe20dfed0d0eff034e5a
Mathew <44036272+Mathew-D@users.noreply.github.com>
2026-06-19 14:23:14 -0400

    Update apps.nix

--- modules/apps.nix
+++ modules/apps.nix
@@ -5,9 +5,15 @@
     kdePackages.kate
     vscode
    # windsurf
-    processing
+    
     libreoffice-fresh
     masterpdfeditor4
+
+   (writeShellScriptBin "processing" ''
+    export _JAVA_OPTIONS="--add-opens=java.desktop/sun.awt.X11=ALL-UNNAMED --add-opens=java.desktop/java.awt=ALL-UNNAMED"
+    exec ${pkgs.processing}/bin/processing "$@"
+  '')
+
   ];
   programs.firefox.enable = true;
 }
