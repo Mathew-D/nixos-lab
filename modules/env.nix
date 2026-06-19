3e7c1412443c66e21feaf75158962cbd9e7fe4bc
Mathew <44036272+Mathew-D@users.noreply.github.com>
2026-06-19 14:21:01 -0400

    Update env.nix

--- modules/env.nix
+++ modules/env.nix
@@ -2,8 +2,8 @@
 
 {
   environment.variables = {
-    _JAVA_OPTIONS =
-      "--add-opens=java.desktop/sun.awt.X11=ALL-UNNAMED --add-opens=java.desktop/java.awt=ALL-UNNAMED";
+  #  _JAVA_OPTIONS =
+  #    "--add-opens=java.desktop/sun.awt.X11=ALL-UNNAMED --add-opens=java.desktop/java.awt=ALL-UNNAMED";
 
     # future variables
     EDITOR = "nano";
