77799bac5b3e435cf139389135edb35e6596609b
Mathew <44036272+Mathew-D@users.noreply.github.com>
2026-06-19 13:34:44 -0400

    Update apps.nix

--- modules/apps.nix
+++ modules/apps.nix
@@ -4,6 +4,10 @@
   environment.systemPackages = with pkgs; [ 
     kdePackages.kate
     vscode
+    windsurf
+    processing
+    libreoffice-fresh
+    masterpdfeditor4
   ];
   programs.firefox.enable = true;
 }
