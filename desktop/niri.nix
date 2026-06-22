704c1d8f5c94afac2f093d7c1b2742b9dd103e62
Mathew-D <mathew_dusome@kprdsb.ca>
2026-06-22 08:23:43 -0400

    portal

--- desktop/niri.nix
+++ desktop/niri.nix
@@ -3,5 +3,10 @@
 {
 programs.niri.enable = true;
 services.gnome.gnome-keyring.enable = true; 
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
 }
 
