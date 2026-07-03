1ec468e9fa3500e1443d767b70ea13055b388835
Mathew-D <mathew_dusome@kprdsb.ca>
2026-07-03 02:18:23 -0400

    greeter

--- display/greetd.nix
+++ display/greetd.nix
@@ -4,16 +4,30 @@
   inputs.noctalia-greeter.nixosModules.default
 ];
 
+  users.groups.greeter = {};
+  users.users.greeter = {
+    isSystemUser = true;
+    group = "greeter";
+    home = "/var/lib/noctalia-greeter";
+    createHome = true;
+  };
+
+  services.greetd.settings.default_session.user = "greeter";
+
 programs.noctalia-greeter = {
   enable = true;
   package = inputs.noctalia-greeter.packages.${pkgs.stdenv.hostPlatform.system}.default;
 
   # Optional configuration
   greeter-args = "";
-  settings.cursor = {
-    theme = "Adwaita";
-    size = 24;
-    package = pkgs.adwaita-icon-theme;
+  settings = {
+    cursor = {
+      theme = "Adwaita";
+      size = 24;
+    };
+    keyboard = {
+      layout = "us";
+    };
   };
 };
 }
