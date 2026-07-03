dcc22a953631adffe7de84e4202c5701b475938b
Mathew-D <mathew_dusome@kprdsb.ca>
2026-07-03 01:51:29 -0400

    cachix

--- base.nix
+++ base.nix
@@ -15,10 +15,16 @@
   ];
 
 # Turn flakes on
- nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+ nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    extra-substituters = [ "https://noctalia.cachix.org" ];
+    extra-trusted-public-keys = [
+      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+    ];
+  };
 
 
   # Bootloader
