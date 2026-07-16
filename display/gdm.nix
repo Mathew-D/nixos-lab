bbf42a7cc9e7d0e810399e9bf8eb3ce65d86beef
Mathew-D <mathew_dusome@kprdsb.ca>
2026-07-16 16:50:17 -0400

    gdm test

--- display/gdm.nix
+++ display/gdm.nix
@@ -1,32 +1,25 @@
 { config, pkgs, ... }:
 {
   services.displayManager.gdm.enable = true;
- 
-  
-security.pam.services.gdm-password.rules.session.mkHome = {
-  order = 120;
-  control = "optional";
-  modulePath = "${pkgs.linux-pam}/lib/security/pam_mkhomedir.so";
-  args = [
-    "skel=/etc/skel"
-    "umask=0077"
-  ];
-};
-/*
-programs.dconf.profiles.gdm.databases = [
-  {
-    settings = {
-      "org/gnome/login-screen" = {
-        disable-user-list = true;
-      };
-      #"org/gnome/desktop/background" = {
-      #  picture-uri = "file://${./gdm-background.png}";
-      #  picture-uri-dark = "file://${./gdm-background.png}";
-      #  picture-options = "zoom";
-      #};
-    };
-  }
-];
-*/
+
+  security.pam.services.gdm-password.rules.session.mkHome = {
+    order = 120;
+    control = "optional";
+    modulePath = "${pkgs.linux-pam}/lib/security/pam_mkhomedir.so";
+    args = [
+      "skel=/etc/skel"
+      "umask=0077"
+    ];
+  };
+
+  environment.etc."gdm/background.png".source = ./gdm-background.png;
+
+  environment.etc."gdm/custom.css".text = ''
+    #lockDialogGroup {
+      background: url(file:///etc/gdm/background.png);
+      background-size: cover;
+      background-position: center;
+    }
+  '';
 }
 
