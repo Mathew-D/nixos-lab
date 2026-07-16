95c5aeec9ced1a48a0e90324f33eb2a9a37780e5
Mathew-D <mathew_dusome@kprdsb.ca>
2026-07-16 16:17:36 -0400

    gdm

--- display/gdm.nix
+++ display/gdm.nix
@@ -3,7 +3,7 @@
   services.displayManager.gdm.enable = true;
  
   
- security.pam.services.gdm-password.rules.mkHome = {
+security.pam.services.gdm-password.rules.session.mkHome = {
   order = 120;
   control = "optional";
   modulePath = "${pkgs.linux-pam}/lib/security/pam_mkhomedir.so";
