3cfd0321a781a7b1cfd82b8ddf61ab1705f42637
Mathew-D <mathew_dusome@kprdsb.ca>
2026-07-16 16:14:53 -0400

    gdm-test

--- display/gdm.nix
+++ display/gdm.nix
@@ -3,14 +3,14 @@
   services.displayManager.gdm.enable = true;
   services.displayManager.gdm.wayland = true;
   
-  security.pam.services.gdm.rules.session.mkHome = {
-    order = 120;
-    control = "optional";
-    modulePath = "${pkgs.linux-pam}/lib/security/pam_mkhomedir.so";
-    args = [
-      "skel=/etc/skel"
-      "umask=0077"
-    ];
-  };
+ security.pam.services.gdm-password.rules.mkHome = {
+  order = 120;
+  control = "optional";
+  modulePath = "${pkgs.linux-pam}/lib/security/pam_mkhomedir.so";
+  args = [
+    "skel=/etc/skel"
+    "umask=0077"
+  ];
+};
 }
 
