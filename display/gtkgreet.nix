45c22538b23a5f02794f6d1168aafacef2ae2da8
Mathew-D <mathew_dusome@kprdsb.ca>
2026-07-17 18:27:24 -0400

    greetd

--- display/gtkgreet.nix
+++ display/gtkgreet.nix
@@ -1,35 +1,20 @@
 { config, pkgs, lib, ... }:
 
 {
-  # Background image
   environment.etc."greetd/background.png".source = ./gdm-background.png;
 
-  # gtkgreet CSS
   environment.etc."greetd/gtkgreet.css".text = ''
     window {
       background-image: url("file:///etc/greetd/background.png");
-      background-size: contain;
+      background-size: cover;
       background-position: center;
-      background-repeat: no-repeat;
     }
 
     * {
       color: white;
-      font-size: 18px;
-    }
-
-    entry {
-      color: white;
-      background-color: rgba(0, 0, 0, 0.5);
-    }
-
-    button {
-      color: white;
-      background-color: rgba(0, 0, 0, 0.5);
     }
   '';
 
-  # Commands shown in gtkgreet session dropdown
   environment.etc."greetd/environments".text = ''
     dbus-run-session startplasma-wayland
   '';
@@ -48,8 +33,7 @@
       command = ''
         ${pkgs.cage}/bin/cage -s -- \
           ${pkgs.gtkgreet}/bin/gtkgreet \
-          -b /etc/greetd/background.png \
-          -l /etc/greetd/gtkgreet.css
+          -s /etc/greetd/gtkgreet.css
       '';
     };
   };
