6467b2c93131d10ed2ff3b6f15a43f053c69f860
Mathew-D <mathew_dusome@kprdsb.ca>
2026-07-17 17:11:16 -0400

    regreet

--- display/regreet.nix
+++ display/regreet.nix
@@ -2,13 +2,22 @@
 
 {
 
+
+  # FreeIPA / SSSD
+  services.sssd.enable = true;
+
+
   # greetd + regreet
   services.greetd = {
     enable = true;
 
     settings = {
       default_session = {
-        command = "${pkgs.greetd.regreet}/bin/regreet";
+        # regreet needs a Wayland compositor
+        command = ''
+          ${pkgs.cage}/bin/cage -s -- ${pkgs.greetd.regreet}/bin/regreet
+        '';
+
         user = "greeter";
       };
     };
@@ -22,7 +31,7 @@
     settings = {
       background = {
         path = "/etc/greetd/background.png";
-        fit = "Cover";
+        fit = "cover";
       };
 
       appearance = {
@@ -31,35 +40,35 @@
 
       GTK = {
         application_prefer_dark_theme = true;
-        cursor_theme_name = "Adwaita";
       };
     };
   };
 
 
-  # Put your wallpaper into the immutable system
+  # Background image
   environment.etc."greetd/background.png".source = ./gdm-background.png;
 
 
-  # Needed packages
+  # Required packages
   environment.systemPackages = with pkgs; [
-    regreet
+    cage
+    greetd.regreet
     adwaita-icon-theme
   ];
 
 
-  # SSSD / FreeIPA
-  services.sssd.enable = true;
-
-
-  # PAM support
+  # PAM integration
   security.pam.services.greetd = {
     enableGnomeKeyring = true;
   };
 
 
-  # Optional: make sure Wayland variables exist
+  # Wayland environment
   environment.sessionVariables = {
     XDG_SESSION_TYPE = "wayland";
   };
+
+
+  # Niri session
+  programs.niri.enable = true;
 }
