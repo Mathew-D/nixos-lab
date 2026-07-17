8ce8b14136eacc6f1cf1cfec6a2e2c71c99a09df
Mathew-D <mathew_dusome@kprdsb.ca>
2026-07-17 17:51:01 -0400

    gtkgreet

--- display/gtkgreet.nix
+++ display/gtkgreet.nix
@@ -1,25 +1,44 @@
 { config, pkgs, lib, ... }:
 
 {
+  # Background image for gtkgreet
+  environment.etc."greetd/background.png".source = ./gdm-background.png;
+
+  # GTK CSS for gtkgreet
+  environment.etc."greetd/gtkgreet.css".text = ''
+    window {
+      background-image: url("/etc/greetd/background.png");
+      background-size: cover;
+      background-position: center;
+    }
+
+    #user_entry,
+    #password_entry {
+      font-size: 18px;
+    }
+
+    button {
+      font-size: 18px;
+    }
+  '';
+
   environment.systemPackages = with pkgs; [
     gtkgreet
     cage
   ];
-environment.etc."greetd/background.png".source = ./gdm-background.png;
+
   services.greetd = {
     enable = true;
 
     settings = {
       default_session = {
         user = "greeter";
+
         command = ''
-  ${pkgs.cage}/bin/cage -s -- \
-    ${pkgs.greetd.gtkgreet}/bin/gtkgreet \
-      --fullscreen \
-      --background /etc/greetd/background.png \
-      --time \
-      --cmd "dbus-run-session ${pkgs.niri}/bin/niri-session"
-'';
+          ${pkgs.cage}/bin/cage -s -- \
+            ${pkgs.gtkgreet}/bin/gtkgreet \
+              -c /etc/greetd/gtkgreet.css
+        '';
       };
     };
   };
