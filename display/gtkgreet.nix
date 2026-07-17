483dd0fbb61845724b414706d95949c7b722c514
Mathew-D <mathew_dusome@kprdsb.ca>
2026-07-17 18:43:08 -0400

    gtkgreet

--- display/gtkgreet.nix
+++ display/gtkgreet.nix
@@ -33,7 +33,6 @@
 
       command = ''
         ${pkgs.cage}/bin/cage -s -- \
-         env LANG=en_US.UTF-8 LC_TIME=en_US.UTF-8 \
           ${pkgs.gtkgreet}/bin/gtkgreet \
           -s /etc/greetd/gtkgreet.css \
           -c "dbus-run-session startplasma-wayland"
