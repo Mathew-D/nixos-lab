f15191e5a63390d621f2e5347c9b25ac36ff137c
Mathew-D <mathew_dusome@kprdsb.ca>
2026-07-17 19:20:51 -0400

    greetd homes

--- display/regreet.nix
+++ display/regreet.nix
@@ -6,6 +6,15 @@
   # FreeIPA / SSSD
   services.sssd.enable = true;
 
+security.pam.services.greetd.rules.session.mkHome = {
+  order = 120;
+  control = "optional";
+  modulePath = "${pkgs.linux-pam}/lib/security/pam_mkhomedir.so";
+  args = [
+    "skel=/etc/skel"
+    "umask=0077"
+  ];
+};
 
   # greetd + regreet
   services.greetd = {
