993be22b2ff7928e2d36124a4717ca92ae0452b8
Mathew-D <mathew_dusome@kprdsb.ca>
2026-09-16 15:02:37 -0400

    greeter

--- display/greetd.nix
+++ display/greetd.nix
@@ -50,7 +50,7 @@
 
   services.greetd.settings.default_session.user = "greeter";
 
-  programs.noctalia-greeter = {
+  services.displayManager.noctalia-greeter = {
     enable = true;
     package = inputs.noctalia-greeter.packages.${pkgs.stdenv.hostPlatform.system}.default;
 
